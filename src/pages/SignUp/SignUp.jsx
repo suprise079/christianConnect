82930a66303e0e696fd68fdcb583c92a00a98a54
Heritier Kaumbu <rainheritier@gmail.com>
2021-09-09 14:39:08 +0000

    nice

--- src/pages/SignUp/SignUp.jsx
+++ src/pages/SignUp/SignUp.jsx
@@ -1,4 +1,12 @@
-import { IonButton, IonPage, IonBackButton, IonButtons, IonHeader, IonToolbar, IonTitle } from "@ionic/react";
+import {
+  IonInput,
+  IonPage,
+  IonBackButton,
+  IonButtons,
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+} from "@ionic/react";
 import "../../pages/StylesForPages.css";
 import styled from "styled-components";
 
@@ -6,20 +14,85 @@ const Body = styled(IonPage)`
   position: relative;
   width: 100%;
   height: 100%;
-  background: white;
+  background: rgb(164, 204, 185);
+  background: linear-gradient(
+    0deg,
+    rgba(164, 204, 185, 1) 26%,
+    rgba(52, 141, 99, 1) 60%,
+    rgba(52, 141, 99, 1) 100%
+  );
   box-sizing: border-box;
+  z-index: -1;
 `;
 
 const SignUp = () => {
   return (
     <Body>
-      {/* <div id="topBar"><IonButton routerLink="/home" id="backButton">Hello</IonButton></div> */}
-      <IonHeader className="ion-no-border"><IonToolbar >
-        <IonButtons slot="start">
-          <IonBackButton defaultHref="/" />
-        </IonButtons>
-        <IonTitle>Register your fellowship</IonTitle>
-      </IonToolbar></IonHeader> 
+      <IonHeader color="inherit" className="ion-no-border">
+        <IonToolbar>
+          <IonButtons slot="start">
+            <IonBackButton defaultHref="/" />
+          </IonButtons>
+          <IonTitle>Register your fellowship</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+
+      <div id="inputs">
+        <form action="/" method="post">
+          <label htmlFor="name">Name of fellowship</label>
+          <IonInput
+            name="name"
+            clearInput="true"
+            className="inputField"
+          ></IonInput>
+
+          <label htmlFor="First name">First name</label>
+          <IonInput
+            name="First name"
+            clearInput="true"
+            className="inputField"
+          ></IonInput>
+
+          <label htmlFor="Last name">Last name</label>
+          <IonInput
+            name="Last name"
+            clearInput="true"
+            className="inputField"
+          ></IonInput>
+
+          <label htmlFor="Phone number">Phone number</label>
+          {/* will use regex validation to format to NUMBERS ONLY */}
+          <IonInput
+            name="Phone number"
+            clearInput="true"
+            className="inputField"
+          ></IonInput>
+
+          <label htmlFor="Email address">Email address</label>
+          <IonInput
+            type="email"
+            name="Email address"
+            clearInput="true"
+            className="inputField"
+          ></IonInput>
+
+          <label htmlFor="Password">Password</label>
+          <IonInput
+            type="password"
+            clearInput="true"
+            className="inputField"
+          ></IonInput>
+
+          <label htmlFor="Confirm password">Confirm password</label>
+          <IonInput
+            type="password"
+            name="Confirm password"
+            clearInput="true"
+            className="inputField"
+          ></IonInput>
+          <input type="submit" value="Create a Fellowship"/>
+        </form>
+      </div>
     </Body>
   );
 };
