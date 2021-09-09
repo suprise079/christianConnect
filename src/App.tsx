51e4978f3b9ff03f9f14cab09ee10f25ce9ecdd3
Malebo Moleleki <75163080+malebo19@users.noreply.github.com>
2021-09-09 12:52:24 +0200

    Tab bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,57 +1,11 @@
-import { Redirect, Route } from 'react-router-dom';
-import {
-  IonApp,
-  IonIcon,
-  IonLabel,
-  IonRouterOutlet,
-  IonTabBar,
-  IonTabButton,
-  IonTabs,
-} from '@ionic/react';
-import { IonReactRouter } from '@ionic/react-router';
-import { albums } from 'ionicons/icons';
-import Tab2 from './pages/Tab2';
-
-
-/* Core CSS required for Ionic components to work properly */
-// import '@ionic/react/css/core.css';
-
-/* Basic CSS for apps built with Ionic */
-// import '@ionic/react/css/normalize.css';
-// import '@ionic/react/css/structure.css';
-// import '@ionic/react/css/typography.css';
-
-/* Optional CSS utils that can be commented out */
-// import '@ionic/react/css/padding.css';
-// import '@ionic/react/css/float-elements.css';
-// import '@ionic/react/css/text-alignment.css';
-// import '@ionic/react/css/text-transformation.css';
-// import '@ionic/react/css/flex-utils.css';
-// import '@ionic/react/css/display.css';
-
-/* Theme variables */
-// import './theme/variables.css';
+import {IonApp } from '@ionic/react';
+import TabBar from './tabBar';
 
 const App: React.FC = () => (
   <IonApp>
-    <IonReactRouter>
-      <IonTabs>
-        <IonRouterOutlet>
-          
-          <Route exact path="/tab2">
-            <Tab2 />
-          </Route>
-          
-          
-        </IonRouterOutlet>
-        <IonTabBar slot="bottom">
-          <IonTabButton tab="tab2" href="/tab2">
-            <IonIcon icon={albums} />
-            <IonLabel>Subscription</IonLabel>
-          </IonTabButton>
-        </IonTabBar>
-      </IonTabs>
-    </IonReactRouter>
+
+    <TabBar />
+
   </IonApp>
 );
 
