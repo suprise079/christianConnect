7a3f628f00c1cae40559f5a002c5a6ad7dac507f
suprise079 <ns.ngoveni079@gmail.com>
2021-09-20 22:57:33 +0200

    updated subscriptions

--- src/pages/subscription/annocements.tsx
+++ src/pages/subscription/annocements.tsx
@@ -1,5 +1,6 @@
 import { IonContent, IonHeader, IonCard, IonCardHeader, IonCardSubtitle, IonCardTitle, IonCardContent, IonPage,IonList, IonTitle, IonToolbar, IonButtons, IonButton, IonIcon } from '@ionic/react';
 import { arrowBack } from 'ionicons/icons';
+import { useState } from 'react';
 import ExploreContainer from '../../components/ExploreContainer';
 import './annocements.css'
 
@@ -7,19 +8,48 @@ import './annocements.css'
 const Annoce: React.FC = () => {
 
     const loop = [1,2,3,4, 5,6];
+    const [selectedId, setSelectedId] = useState(null)
+    const annocements = [
+      {title:'Gents night', content:'Rev SC & PA Mathebula invites all Men to the first Gents Night on the year under the theme, GENTS let’s talk, unpacking it all.', date:'21 May 2021 18:30', id:1},
+      {title:'Mens conference', content:'Rev SC & PA Mathebula invites all Men to the first Gents Night on the year under the theme, GENTS let’s talk, unpacking it all.', date:'21 May 2021 18:30', id:2},
+      {title:'Teens camp', content:'Rev SC & PA Mathebula invites all Men to the first Gents Night on the year under the theme, GENTS let’s talk, unpacking it all.', date:'21 May 2021 18:30', id:3},
+      {title:'Prayer night', content:'Rev SC & PA Mathebula invites all Men to the first Gents Night on the year under the theme, GENTS let’s talk, unpacking it all.', date:'21 May 2021 18:30', id:4},
+      {title:'Youth feast', content:'Rev SC & PA Mathebula invites all Men to the first Gents Night on the year under the theme, GENTS let’s talk, unpacking it all.', date:'21 May 2021 18:30', id:5},
+      {title:'Worship night', content:'Rev SC & PA Mathebula invites all Men to the first Gents Night on the year under the theme, GENTS let’s talk, unpacking it all.', date:'21 May 2021 18:30', id:6}
+    ]
+    function readMore(event:any, cardId:string, contentId:string) {
+        var card = document.getElementById(cardId)!
+        var content = document.getElementById(contentId)!
+        var button = event.target
+        console.log(cardId, contentId)
+        console.log('running')
+
+        if (button.innerHTML == 'More Info'){
+          card.style.transform = 'scale(1.05)';
+          content.style.height = 'fit-content'
+          button.innerHTML = 'Done';
+        }
+        else{
+          card.style.transform = 'scale(1)';
+          content.style.transition = 'height 5s'
+          content.style.height = '5em'
+          button.innerHTML = 'More Info';
+        }
+    }
 
     return (
         <>
+        
             <IonList id="listAnnocement">
-            {loop.map((e) => {
+            {annocements.map((annocemnet) => {
+              var id = annocemnet.id.toString();
               return(
-              <IonCard key={e} className='p-2' >
-                <IonCardTitle className='text-center m-0 p-0'>Gents night</IonCardTitle>
-                <IonCardContent className='text-center m-0 p-1'>Rev SC & PA Mathebula invites all Men to the first Gents Night
-    on the year under the theme, GENTS let’s talk, unpacking it all.</IonCardContent>
+              <IonCard key={annocemnet.id} id={'card'+id} className='p-2' >
+                <IonCardTitle className='text-center m-0 p-0'>{annocemnet.title}</IonCardTitle>
+                <IonCardContent className='text-center m-0 p-1 summary' id={'content'+id}>{annocemnet.content}</IonCardContent>
                 <div className='timeBtn' >
-                  <IonCardSubtitle> 21 May 2021 18:30</IonCardSubtitle>
-                  <IonButton  className='p-1' style={{backgroundColor:'CBF8FF'}} >More Info</IonButton>
+                  <IonCardSubtitle>{annocemnet.date}</IonCardSubtitle>
+                  <IonButton  className='p-1' style={{backgroundColor:'CBF8FF'}} onClick={(e) => {readMore(e,'card'+id,'content'+id)}} >More Info</IonButton>
                 </div>
                 
               </IonCard>
--- src/pages/subscription/devotions.tsx
+++ src/pages/subscription/devotions.tsx
@@ -1,27 +1,62 @@
 import { IonCard, IonButton, IonCardContent, IonCardSubtitle } from "@ionic/react";
 import fearless from './fearless.jpg';
-import play from './play.jpg'
+// import Video from "./dummy.mp4"
 import './devotions.css'
 
 const Devotions: React.FC = () => {
 
     const loop = [1,2,3,4, 5, 6];
 
+    function readMore(event:any){
+        var content = document.getElementById('devotionContent')!
+        var button = event.target
+        var card = document.getElementById('devotionCard')!
+        console.log(content)
+
+
+        if (button.innerHTML == 'Read'){
+            card.style.transform = 'scale(1.05)';
+            content.style.height = 'fit-content'
+            button.innerHTML = 'Done';
+        }
+        else {
+            content.style.height = '11em';
+            button.innerHTML = 'Read';
+            card.style.transform = 'scale(1)';
+        }
+    }
+
     return ( 
         <div className='body '>
             <h6>Daily devotions:</h6>
-            <IonCard className='text-center p-2'>
+            <IonCard className='text-center p-2' id='devotionCard'>
                 <div className='devoPic'>
                     <img src={fearless} alt="example.png" /><br />
                     
                 </div>
                 <div className='massage p-2'>
-                    <h3 className='p-0 m-0'>Fearless</h3>
-                    <IonCardContent className='p-1'>
-                    True remedy against tormenting fear such as the psalmist describes above is faith in God. Strong faith can give you victory over fear no matter what the circumstances are...
+                    <h3 className='p-0 m-0'>The word of our God stands forever</h3>
+                    <IonCardContent className='p-1' id='devotionContent' >
+                    The grass withers and the flowers fade, but the word of our God stands forever, Isaiah 40:8
+
+ 
+
+Think about all the things around you right now. Look around you and see all the things that will fade away, grass, flowers, all kinds of things in nature and the world around us are going to fade. Think about some of the most basic things you find pleasure in right now - like that favourite show on TV -  that are going to fade. Like grass, all things come to an end.
+
+That’s what God is saying through Isaiah, look at all the things that fade in the world. Then, look at the Word of God and know it will never fade. It will stand forever. God wants you to realize the wonder of His word and the value of spending time in it.
+
+This Word shows you how to walk right. This Word shows you how to act right. This Word shows you how to live right. You can hide this Word in your heart, so that you won’t have to sin against the Lord (Psalm 119:11). This Word can be a lamp unto your feet and a light unto your path (Psalm 119:105).
+
+Everything you and I need to know is in the Bible. The teachings of God, the promises of God, are all spelled out in the word of God. The word of God is true, and it never changes.
+
+Live today for that which will matter forever by spending time in God’s everlasting Word.  Remember what Jesus said, do not establish your life on things that moth and rust can consume or where robbers can break in and steal. Instead, establish your life on things that last forever (Matthew 6:19).  
+
+Can I ask you to look deep within yourself and ask yourself this question: Am I living this life in accordance with the one true foundation? Am I living life accordance with my faith?
+
+Live today for that which is going to matter forever. Spend time in God’s word. The Word has stood the test of time and surely shown that it is here to outlive whatever struggles of life you are faced with.
                     </IonCardContent>
                 </div>
-                <IonButton style={{backgroundColor:'#8EB9A7'}}>Read</IonButton>
+                <IonButton style={{backgroundColor:'#8EB9A7'}} onClick={(e) => readMore(e)}>Read</IonButton>
             </IonCard>
 
             <h5>Sermons:</h5>
@@ -29,7 +64,9 @@ const Devotions: React.FC = () => {
                 {loop.map((e) =>{
                 return(
                     <IonCard key={e} className='sermon'>
-                        <img src={play} alt="sermon.png" />
+                        <video controls={true} src="./dummy.mp4">
+    Your browser does not support the HTML5 Video element.
+                        </video>
                         <IonCardSubtitle ><b>Fearless Man</b></IonCardSubtitle>
                     </IonCard>
                 )
