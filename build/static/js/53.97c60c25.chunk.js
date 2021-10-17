1f0e6c9a35fe12ed0f456e65adcb83a0cd221df2
kibambe-0167 <kibambe.0167@gmail.com>
2021-10-17 15:36:31 +0200

    ksjdfbskjdbfksjdbf

---
+++ build/static/js/53.97c60c25.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpchristianConnect=this.webpackJsonpchristianConnect||[]).push([[53],{167:function(n,t,e){"use strict";e.r(t),e.d(t,"startStatusTap",(function(){return o}));var i=e(23),c=e(21),o=function(){var n=window;n.addEventListener("statusTap",(function(){Object(i.h)((function(){var t=n.innerWidth,e=n.innerHeight,o=document.elementFromPoint(t/2,e/2);if(o){var r=o.closest("ion-content");r&&new Promise((function(n){return Object(c.c)(r,n)})).then((function(){Object(i.f)((function(){return r.scrollToTop(300)}))}))}}))}))}}}]);
+//# sourceMappingURL=53.97c60c25.chunk.js.map
