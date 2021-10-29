492f4cbb1e38ce65a839e47e5dc22dda723e7c0c
kibambe-0167 <kibambe.0167@gmail.com>
2021-10-29 06:49:08 +0200

    leader can get their fellowship data and normal data

--- build/static/js/53.f0455bfc.chunk.js
+++
@@ -1,2 +0,0 @@
-(this.webpackJsonpchristianConnect=this.webpackJsonpchristianConnect||[]).push([[53],{188:function(n,t,e){"use strict";e.r(t),e.d(t,"startStatusTap",(function(){return o}));var i=e(28),c=e(25),o=function(){var n=window;n.addEventListener("statusTap",(function(){Object(i.h)((function(){var t=n.innerWidth,e=n.innerHeight,o=document.elementFromPoint(t/2,e/2);if(o){var r=o.closest("ion-content");r&&new Promise((function(n){return Object(c.c)(r,n)})).then((function(){Object(i.f)((function(){return r.scrollToTop(300)}))}))}}))}))}}}]);
-//# sourceMappingURL=53.f0455bfc.chunk.js.map
